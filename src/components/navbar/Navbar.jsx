277d6df75af442b53ca8035a5472c8b9570d8782
drypzz <fdfjonfmd@gmail.com>
2023-07-04 21:31:37 -0300

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@
 import NavItem from './NavItem';
 import { useState } from 'react';
 
-import { FaHome, FaShieldAlt, FaShareAlt, FaPhotoVideo, FaMoneyBillAlt, FaPhoneSquareAlt, FaUserPlus } from 'react-icons/fa';
+import { FaHome, FaShieldAlt, FaShareAlt, FaPhotoVideo, FaMoneyBillAlt, FaPhoneSquareAlt } from 'react-icons/fa';
 
 const MENU_LIST = [
     {text: ' Inicio', href: '/', icon: <FaHome />},
@@ -32,7 +32,7 @@ const Navbar = ({bool}) => {
                         <span className='logo__title'>GUNS AIRSOFT ARENA</span>
                     </div>
                     <div>
-                        <a target='_blank' href={`https://api.whatsapp.com/send?phone=554797519814&text=${encodeURIComponent('Olá! Gostaria de marcar um game, quais os valores?')}`} className='link__title'><FaUserPlus /> Reserve Agora</a>
+                        <a target='_blank' href={`https://api.whatsapp.com/send?phone=554797519814&text=${encodeURIComponent('Olá! Gostaria de marcar um game, quais os valores?')}`} className='link__title'>Reserve Agora</a>
                     </div>
                 </div>
             </section>
@@ -57,7 +57,7 @@ const Navbar = ({bool}) => {
                             })
                         }
                         <div className='link__div'>
-                            <a target='_blank' href={`https://api.whatsapp.com/send?phone=554797519814&text=${encodeURIComponent('Olá! Gostaria de marcar um game, quais os valores?')}`} className='link__title'><FaUserPlus /> Reserve Agora</a>
+                            <a target='_blank' href={`https://api.whatsapp.com/send?phone=554797519814&text=${encodeURIComponent('Olá! Gostaria de marcar um game, quais os valores?')}`} className='link__title'>Reserve Agora</a>
                         </div>
                     </div>
                 </nav>
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -10,10 +10,23 @@ import LoadingScreen from '@/components/loading/Loading';
 
 import '../utils/contact.css';
 
+import { FaInstagram, FaWhatsapp, FaFacebook, FaMap, FaPhoneAlt, FaRegEnvelope } from 'react-icons/fa';
+
 const Contacts = () => {
     const [loading, setLoading] = useState(true);
+    const [map, setMap] = useState([]);
+
+    async function loadMap(){
+        const result = fetch('https://maps.google.com/maps?q=Guns+Airsoft+Arena&t=&z=17&ie=UTF8&iwloc=&output=embed')
+            .then((res) => setMap(res.url))
+            .catch((err) => setMap(<div><h1>{err}</h1></div>)
+        );
+
+        return result;
+    }
   
     useEffect(() => {
+        loadMap();
         setTimeout(() => {
             setLoading(false);
         }, 2000);
@@ -38,6 +51,81 @@ const Contacts = () => {
                     </Head>
                     <Navbar bool={5} />
                     <CustomNav link='/' text='Contato' />
+                    <div className='main-contacts'>
+                        <div className='title'>
+                            <h1>Localização</h1>
+                        </div>
+                        <div className='location'>
+                            <iframe src={map} frameborder='0'></iframe>
+                        </div>
+                        <div className='contacts'>
+                            <div className='contacts-container'>
+                                <div className='contacts-info'>
+                                    <h3 className='title--info'>Contato</h3>
+                                    <ul id='kasw' className='contacts-info-item--ul'>
+                                        <li>
+                                            <a target='_blank' href='https://goo.gl/maps/cxPno5J1cjJEvS1W6'><FaMap />R. Boehmerwald, 170 - Boehmerwald, Joinville - SC, 89219-731</a>
+                                        </li>
+                                        <li>
+                                            <a href='tel:+5547997519814'><FaPhoneAlt />(47) 99751-9814</a>
+                                        </li>
+                                        <li>
+                                            <a target='_blank' href='mailto:gunsairsoftarena@gmail.com'><FaRegEnvelope />gunsairsoftarena@gmail.com</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div className='contacts-info'>
+                                    <h3 className='title--info'>Horario de Funcionamento</h3>
+                                    <ul className='contacts-info-item--ul'>
+                                        <li>
+                                            Segunda-Feira
+                                            <span>07:00 - 22:00</span>
+                                        </li>
+                                        <li>
+                                            Terça-Feira
+                                            <span>07:00 - 22:00</span>
+                                        </li>
+                                        <li>
+                                            Quarta-Feira
+                                            <span>07:00 - 22:00</span>
+                                        </li>
+                                        <li>
+                                            Quinta-Feira
+                                            <span>07:00 - 22:00</span>
+                                        </li>
+                                        <li>
+                                            Sexta-Feira
+                                            <span>07:00 - 22:00</span>
+                                        </li>
+                                        <li>
+                                            Sábado
+                                            <span>07:00 - 22:00</span>
+                                        </li>
+                                        <li>
+                                            Domingo
+                                            <span>07:00 - 22:00</span>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div className='contacts-info'>
+                                    <h3 className='title--info'>NOS ENCONTRE</h3>
+                                    <div className='container--red'>
+                                        <div className='itens--red'>
+                                            <a target='_blank' href='https://www.instagram.com/gunsairsoftarena/'><FaInstagram/></a>
+                                        </div>
+                                        <div className='itens--red'>
+                                            <a target='_blank' href='https://www.facebook.com/gunsairsoftarena/'><FaFacebook/></a>
+                                        </div>
+                                        <div className='itens--red'>
+                                            <a target='_blank' href='https://api.whatsapp.com/send?phone=554797519814'><FaWhatsapp/></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <Footer />
                 </div>
             }
