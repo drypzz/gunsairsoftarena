06f7dc8433cd9adb9e340b4fcafba0f5016d566b
drypzz <fdfjonfmd@gmail.com>
2024-08-01 17:34:55 -0300

    fix: Fixed code

--- src/app/screens/contato/layout.tsx
+++ src/app/screens/contato/layout.tsx
@@ -1,35 +1,53 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://dev-gunsairsoftarena.netlify.app/contato"),
+  metadataBase: new URL("https://dev-gunsairsoftarena.netlify.app/contato"),
+  title: "Guns Airsoft Arena - Contato",
+  applicationName: "Guns Airsoft Arena",
+  description: "Sua arena de Airsoft em Joinville - SC",
+  creator: "DRYPZZ",
+  authors: [{ name: "DRYPZZ", url: "https://drypzz.netlify.app" }],
+  generator: "NextJS",
+  keywords: [
+    "Drypzz",
+    "Drypzz Dev",
+    "Drypzz Developer",
+    "Drypzz Dev Portfolio",
+    "Guns Airsoft Arena",
+    "Guns Airsoft Arena - Contato",
+    "airsoft",
+    "gunsairsoftarena",
+    "guns airsoft",
+    "arena joinville",
+    "airsoft joinville",
+    "joinville airsoft",
+    "SC Airsoft",
+    "Airsoft",
+    "Guns Arena",
+    "Sua Arena de Airsoft em Joinville"
+  ],
+  twitter: {
+    site: "@gunsairsoftarena",
+    card: "summary_large_image",
+    images: "/logo/transparent.png",
+    creator: "@drypzz",
+  },
+  openGraph: {
     title: "@gunsairsoftarena",
-    applicationName: "Guns Airsoft Arena - Contato",
     description: "Sua arena de Airsoft em Joinville - SC",
-    creator: "DRYPZZ",
-    authors: [{ name: "DRYPZZ", url: "https://drypzz.netlify.app" }],
-    generator: "NextJS",
-    keywords: ["Guns Airsoft Arena - Contato", "airsoft", "gunsairsoftarena", "guns airsoft", "arena joinville", "airsoft joinville", "joinville airsoft", "SC Airsoft", "Airsoft", "Guns Arena", "Sua Arena de Airsoft em Joinville"],
-    twitter: {
-        site: "@gunsairsoftarena",
-        card: "summary",
-        images: "/logo/transparent.png",
-    },
-    openGraph: {
-        title: "Guns Airsoft Arena - Contato",
-        description: "Sua arena de Airsoft em Joinville - SC",
-        siteName: "Guns Airsoft Arena - Contato",
-        type: "website",
-        url: "https://dev-gunsairsoftarena.netlify.app/contato",
-        images: [{ url: "/logo/transparent.png" }],
-        countryName: "Brazil",
-        locale: "pt_BR",
-    },
+    siteName: "Guns Airsoft Arena - Contato",
+    type: "website",
+    url: "https://dev-gunsairsoftarena.netlify.app/contato",
+    images: [{ url: "/logo/transparent.png" }],
+    countryName: "Brazil",
+    locale: "pt_BR",
+  },
 };
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return ({children});
-};
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
--- src/app/screens/valores/layout.tsx
+++ src/app/screens/valores/layout.tsx
@@ -1,35 +1,53 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://dev-gunsairsoftarena.netlify.app/valores"),
+  metadataBase: new URL("https://dev-gunsairsoftarena.netlify.app/valores"),
+  title: "Guns Airsoft Arena - Valores",
+  applicationName: "Guns Airsoft Arena",
+  description: "Sua arena de Airsoft em Joinville - SC",
+  creator: "DRYPZZ",
+  authors: [{ name: "DRYPZZ", url: "https://drypzz.netlify.app" }],
+  generator: "NextJS",
+  keywords: [
+    "Drypzz",
+    "Drypzz Dev",
+    "Drypzz Developer",
+    "Drypzz Dev Portfolio",
+    "Guns Airsoft Arena",
+    "Guns Airsoft Arena - Valores",
+    "airsoft",
+    "gunsairsoftarena",
+    "guns airsoft",
+    "arena joinville",
+    "airsoft joinville",
+    "joinville airsoft",
+    "SC Airsoft",
+    "Airsoft",
+    "Guns Arena",
+    "Sua Arena de Airsoft em Joinville"
+  ],
+  twitter: {
+    site: "@gunsairsoftarena",
+    card: "summary_large_image",
+    images: "/logo/transparent.png",
+    creator: "@drypzz",
+  },
+  openGraph: {
     title: "@gunsairsoftarena",
-    applicationName: "Guns Airsoft Arena - Valores",
     description: "Sua arena de Airsoft em Joinville - SC",
-    creator: "DRYPZZ",
-    authors: [{ name: "DRYPZZ", url: "https://drypzz.netlify.app" }],
-    generator: "NextJS",
-    keywords: ["Guns Airsoft Arena - Valores", "airsoft", "gunsairsoftarena", "guns airsoft", "arena joinville", "airsoft joinville", "joinville airsoft", "SC Airsoft", "Airsoft", "Guns Arena", "Sua Arena de Airsoft em Joinville"],
-    twitter: {
-        site: "@gunsairsoftarena",
-        card: "summary",
-        images: "/logo/transparent.png",
-    },
-    openGraph: {
-        title: "Guns Airsoft Arena - Valores",
-        description: "Sua arena de Airsoft em Joinville - SC",
-        siteName: "Guns Airsoft Arena - Valores",
-        type: "website",
-        url: "https://dev-gunsairsoftarena.netlify.app/valores",
-        images: [{ url: "/logo/transparent.png" }],
-        countryName: "Brazil",
-        locale: "pt_BR",
-    },
+    siteName: "Guns Airsoft Arena - Valores",
+    type: "website",
+    url: "https://dev-gunsairsoftarena.netlify.app/valores",
+    images: [{ url: "/logo/transparent.png" }],
+    countryName: "Brazil",
+    locale: "pt_BR",
+  },
 };
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return ({children});
-};
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
